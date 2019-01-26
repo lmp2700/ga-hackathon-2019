405382966bea2b743f9c7dae3dd147c1d37dd278
Liz Steak <lizmaepark@gmail.com>
2019-01-26 13:53:02 -0700

    fixed typos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import LoginContainer from './Containers/LoginContainer'
-import MainContainer from './Containers/MainContainer'
-import SearchContainer from './Containers/MainContainers/SearchContainer'
+import LoginContainer from './Containers/MainContainers/LoginContainer';
+import MainContainer from './Containers/MainContainers/MainContainer';
+import SearchContainer from './Containers/MainContainers/SearchContainer';
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={MainContainer} />
             <Route exact path="/login" component={LoginContainer} />
-            <Router exact path="/search" component={SearchContainer} />
+            <Route exact path="/search" component={SearchContainer} />
             <h1>Maint Container</h1>
           </Switch>
         </BrowserRouter>
